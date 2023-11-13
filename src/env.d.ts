b5fadf1107af95d24416dca4546ca2255355f9e4
Taha <taha.ojari@gmail.com>
2023-11-13 19:53:25 +0330

    feat: use AstroWind template

---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly STRAPI_URL: string;
+}
